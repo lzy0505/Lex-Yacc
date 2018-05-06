9cf1e647c7042ce81964df35f46886e36705e5fe
Stormuzi <3263707107@qq.com>
2018-05-06 22:18:10 +0800

    merge NFAs to NFA.

--- Lex/Lex/suffixToNFA.cpp
+++ Lex/Lex/suffixToNFA.cpp
@@ -19,24 +19,15 @@ int number = 0;
 
 //将后缀表达式转化为NFA
 void suffix_To_NFA(vector<Rules>& suffixRules) {
-	vector<NFA> reNfaVec;//用于存储每个RE转换出来的NFA
+	stack<NFA> nfaStack;//用于构建NFA的工具栈,也是
 	NFA finalNfa;//最终的NFA
-	
 	for (auto &rule : suffixRules) {
 		string pattern = rule.pattern; //已经后缀化后的pattern
-		vector<string>* actions = &rule.actions;//获取操作
-		//ReNFA *nfaArray = new ReNFA[pattern.size()];//保存所有的ReNFA
-		//int lengthOfNfaArray = 0;
-		//state *stateArray = new state[4*pattern.size()];//保存所有的状态
-		//int lengthOfStateArray = 0;
-		stack<NFA> nfaStack;//用于构建NFA的工具栈
 		vector<string> tempActionVec;//临时终态动作数组
-
 		for (auto it = pattern.cbegin() ; it != pattern.cend(); ++it)
 		{
 			NFA upNFA,downNFA;
-			NFAstate nfaState;
-			NFAstate start, end,temp1,temp2;
+			NFAstate start, end,temp1;
 			char now = *it;
 			cout << *it;
 			switch (now) {
@@ -49,16 +40,22 @@ void suffix_To_NFA(vector<Rules>& suffixRules) {
 				start.number = number;
 				++number;
 				end.number = number;
-				++number;
-				downNFA.statesMap.insert(pair<int, NFAstate>(start.number, start));
-				downNFA.statesMap.insert(pair<int, NFAstate>(end.number, end));
+				++number;				
 				//将down的终态和up的终态都连接到end
 				upNFA.statesMap.find(upNFA.endStatesMap.begin()->first)->
 					second.exEdgesMultiMap.insert(pair<char, int>('@', end.number));
 				downNFA.statesMap.find(upNFA.endStatesMap.begin()->first)->
 					second.exEdgesMultiMap.insert(pair<char, int>('@', end.number));
+				//将start连接到down和up的初态
+				start.exEdgesMultiMap.insert(pair<char, int>('@', upNFA.startState));
+				start.exEdgesMultiMap.insert(pair<char, int>('@', downNFA.startState));
+				//连接关系定义好后存入map
+				downNFA.statesMap.insert(pair<int, NFAstate>(start.number, start));
+				downNFA.statesMap.insert(pair<int, NFAstate>(end.number, end));
 				//将upNFA的stateMap拷贝到down中
 				downNFA.statesMap.insert(upNFA.statesMap.begin(), upNFA.statesMap.end());
+				//修改downNFA的初态
+				downNFA.startState = start.number;
 				//修改downNFA的终态
 				downNFA.endStatesMap.clear();
 				downNFA.endStatesMap.insert(pair<int, vector<string> >(end.number, tempActionVec));			
@@ -73,8 +70,6 @@ void suffix_To_NFA(vector<Rules>& suffixRules) {
 				++number;
 				end.number = number;
 				++number;
-				upNFA.statesMap.insert(pair<int, NFAstate>(start.number, start));
-				upNFA.statesMap.insert(pair<int, NFAstate>(end.number, end));
 				//将start和upNFA初态连接
 				start.exEdgesMultiMap.insert(pair<char, int>('@', upNFA.startState));
 				//将start和end连接
@@ -82,6 +77,9 @@ void suffix_To_NFA(vector<Rules>& suffixRules) {
 				//将upNFA的终态和初态连接
 				upNFA.statesMap.find(upNFA.endStatesMap.begin()->first)->
 					second.exEdgesMultiMap.insert(pair<char, int>('@', upNFA.startState));
+				//连接关系定义好后存入map
+				upNFA.statesMap.insert(pair<int, NFAstate>(start.number, start));
+				upNFA.statesMap.insert(pair<int, NFAstate>(end.number, end));
 				//更改终态
 				upNFA.endStatesMap.clear();
 				upNFA.endStatesMap.insert(pair<int, vector<string> >(end.number,tempActionVec));
@@ -113,25 +111,45 @@ void suffix_To_NFA(vector<Rules>& suffixRules) {
 				NFA pushNFA;//用于压栈的NFA
 				//新建一个起始状态
 				start.number = number;
-				pushNFA.statesMap.insert(pair<int, NFAstate>(number, start));
 				++number;
 				//新建一个终止状态
 				end.number = number;
-				++number;
-				pushNFA.statesMap.insert(pair<int, NFAstate>(number, end));
+				++number;	
 				//将起始状态和终止状态连接
 				start.exEdgesMultiMap.insert(pair<char, int>(now, end.number));
+				//连接关系定义好后存入map
+				pushNFA.statesMap.insert(pair<int, NFAstate>(start.number, start));
+				pushNFA.statesMap.insert(pair<int, NFAstate>(end.number, end));
 				//标识当前终态，因此先传入空vector。
 				pushNFA.endStatesMap.insert(pair<int, vector<string>>(end.number, tempActionVec));
 				//压栈
 				nfaStack.push(pushNFA);
 			}
+			
 		}
-		
-		//TODO 把nfaArray，stateArray的内容拷贝到一个nfaArray.size()，stateArray.size() 大小的数组中，防止浪费
-		//TODO 把新的数组存在reNfaVec中，等待合并
-
-		// TODO 合成一个NFA
+		//将action赋给栈顶的NFA的终态
+		nfaStack.top().endStatesMap.begin()->second = rule.actions;
 	}
-
+	//现在得到了装着所有NFA的nfaStack,合并为一个大的NFA。
+	NFA finalNfa =nfaStack.top() , downNFA;
+	nfaStack.pop();
+	NFAstate start;
+	while (!nfaStack.empty()) {
+		//依次，把栈顶NFA与大NFA合并
+		downNFA = nfaStack.top();
+		nfaStack.pop();
+		//新建一个start状态
+		start.number = number;
+		number++;
+		//将start连接到finalNFA和dowNFA的初态
+		start.exEdgesMultiMap.insert(pair<char, int>('@', finalNfa.startState));
+		start.exEdgesMultiMap.insert(pair<char, int>('@', downNFA.startState));
+		//修改finalNFA的起始状态
+		finalNfa.startState = start.number;
+		finalNfa.statesMap.insert(pair<int, NFAstate>(start.number, start));
+		//添加finalNFA的终止状态
+		finalNfa.endStatesMap.insert(*downNFA.endStatesMap.begin());
+		//把downNFA的状态map拷贝到finalNFA中
+		finalNfa.statesMap.insert(downNFA.statesMap.begin(), downNFA.statesMap.end());
+	}
 }
